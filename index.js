e3c3cc334191a90914921a6ab199a3c3357895fe
davejefferson <36189939+davejefferson@users.noreply.github.com>
2018-06-26 13:46:23 +0100

    add basic dropdown menu

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("This is working!");
+
+document.getElementById("menu").addEventListener("click", openMenu);
+
+function openMenu() {
+    document.getElementById("dropdown").classList.toggle("active");
+}
